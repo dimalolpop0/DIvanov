654a59a0fab480379193b8e65693e113f17f6b9e
DmitriyIvanov <124032437+dimalolpop0@users.noreply.github.com>
2023-02-14 11:35:08 +0300

    14/02
    Иванов Дмитрий

---
+++ main (4).py
@@ -0,0 +1,13 @@
+ tseloe = int(10)
+ drobnoe = int(8.4)
+ stroka = ("NO")
+ 
+ big_netseloec = tseloe * 3.5
+ drobnoe = drobnoe - 1
+ print(tseloe // drobnoe)
+ 
+ stroka = ("NoNO_"YesYesYes)
+ print(tseloe)
+ print(drobnoe)
+ print(big_netseloec)
+ print(stroka)
