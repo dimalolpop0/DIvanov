171295e032506e6012fc6fcff95965730cd58ab9
DmitriyIvanov <124032437+dimalolpop0@users.noreply.github.com>
2023-06-02 11:48:59 +0300

    дз на 02.06

---
+++ сезоны года(ДЗ).py
@@ -0,0 +1,13 @@
+a = int(input())
+def season(a):
+    if a == 1 or a == 2 or a == 12:
+        print("Зима")
+    elif a == 3 or a == 4 or a == 5:
+        print("Весна")
+    elif a == 6 or a == 7 or a == 8:
+        print("Лето")
+    elif a == 9 or a == 10 or a ==11:
+        print("Осень")
+    else:
+        print("Нет такого месяца")
+season(a)
